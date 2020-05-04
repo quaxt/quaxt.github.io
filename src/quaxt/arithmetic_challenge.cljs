6c7d7dfb878fd813b1f1d4c70638e97913d7e18d
Michael Reilly <mikeyreilly@gmail.com>
2020-05-04 10:29:43 +0100

    memoize progress function

--- src/quaxt/arithmetic_challenge.cljs
+++ src/quaxt/arithmetic_challenge.cljs
@@ -6,10 +6,10 @@
 
 (defonce app-state (r/atom {}))
 
-(defonce quiz-length 10)
+(def quiz-length 10)
 
-(defn all-questions
-  []
+(defn all-questions[]
+;;(print "all-questions")
   (shuffle (concat
             (for [op ["+" "*"]
                   x (range 1 13)
@@ -22,18 +22,22 @@
                   y (range 1 13)]
               {:op "/" :x (* x y) :y y}))))
 
-(defn sort-by-difficulty [questions]
+(defn sort-by-difficulty[questions]
+;;(print "sort-by-difficulty")
   (let [{:keys [difficulty]} @app-state]
     (sort (fn[a b] (compare (difficulty b 1000000) (difficulty a 1000000)))
           questions)))
 
 (defn make-quiz[n]
-  (take n (sort-by-difficulty (all-questions))))
+;;(print "make-quiz")
+  (shuffle (take n (sort-by-difficulty (all-questions)))))
 
 (defn now[]
+;;(print "now")
   (.now js/Date))
 
 (defn start-new-quiz[n]
+;;(print "start-new-quiz")
   (swap! app-state assoc
          :quiz (make-quiz n)
          :results []
@@ -41,24 +45,28 @@
          :start-time (now)))
 
 (defn question-to-string[{:keys [op x y]} user-answer]
+;;(print "question-to-string")
   (let [op ({"*" "\u00D7"
              "/" "\u00F7"
              "+" "+"
-             "-" "-"} op)]
+             "-" "\u2212"} op)]
     (str x " " op " " y " = " user-answer)))
 
-(defn ask [question user-answer]
+(defn ask[quiz user-answer]
+;;(print "ask")
   [:div
-   {:style {:font-size "13vw"}}
-   (question-to-string question user-answer) "_"])
+   [:div
+    {:style {:font-size "13vw"}}
+    (question-to-string (first quiz) user-answer)  "_"]])
 
 (defn type-key[text]
+;;(print "type-key")
   (swap! app-state update :user-answer str text))
 
 (defn key-div[text on-click]
+;;(print "key-div")
   [:div
-   {:style {:height "10vh"}
-    }
+   {:style {:height "10vh"}}
    
    [:button
     {:style {:height "9vh"
@@ -76,9 +84,11 @@
     text]])
 
 (defn keypad-key[text]
+;;(print "keypad-key")
   [key-div text #(type-key text)])
 
 (defn type-backspace[]
+;;(print "type-backspace")
   (swap! app-state
          update
          :user-answer
@@ -86,19 +96,22 @@
            (subs string 0 (- (count string) 1)))))
 
 (defn backspace-key[]
+;;(print "backspace-key")
   [key-div "\u232B" type-backspace])
 
-(defn as-int
-  [x]
+(defn as-int[x]
+;;(print "as-int")
   (if (string? x)
     (js/parseInt x)
     x))
 
 (defn set-local-storage[key-name value]
+;;(print "set-local-storage")
   (-> (.-localStorage js/window)
       (.setItem key-name value)))
 
 (defn get-local-storage[key-name]
+;;(print "get-local-storage")
   (-> (.-localStorage js/window)
       (.getItem key-name)))
 
@@ -108,21 +121,25 @@
    "*" *
    "/" /})
 
-(defn right? [{:keys [op x y]} user-answer]
+(defn right?[{:keys [op x y]} user-answer]
+;;(print "right?")
   (= ((lookup-op op) x y) user-answer))
 
 (defn difficulty[{:keys [question time user-answer]}]
+;;(print "difficulty")
   (if (right? question user-answer)
     time
     2000000))
 
 (defn read-difficulty-table-from-local-storage[]
+;;(print "read-difficulty-table-from-local-storage")
   (let [difficulty-table-string (get-local-storage "difficulty")
         difficulty-table (if difficulty-table-string
                       (edn/read-string difficulty-table-string) {})]
     (swap! app-state assoc :difficulty difficulty-table)))
 
 (defn compute-difficulty-table[results]
+;;(print "compute-difficulty-table")
   (let [old-results (:difficulty @app-state)
         result-map (into {} (map (fn[result]
                                    (difficulty result)
@@ -131,10 +148,12 @@
     (merge old-results result-map)))
 
 (defn update-difficulty-table[results]
+;;(print "update-difficulty-table")
   (let [difficulty-table (compute-difficulty-table results)]
     (swap! app-state assoc :difficulty difficulty-table)))
 
 (defn type-enter[]
+;;(print "type-enter")
   (let [{:keys [user-answer quiz results start-time]} @app-state]
     (if (first quiz)
       (when-not (= user-answer "")
@@ -154,9 +173,11 @@
         (start-new-quiz quiz-length)))))
 
 (defn enter-key[font-size]
+;;(print "enter-key")
   [key-div "\u23CE" type-enter])
 
 (defn keypad[]
+;;(print "keypad")
   [:table
    {:style {:width "100%" :table-layout " fixed"}}
    [:tbody
@@ -168,6 +189,7 @@
      [:td [enter-key]]]]])
 
 (defn right-or-wrong-td[style question user-answer]
+;;(print "right-or-wrong-td")
   (let [right (right? question user-answer)]
     (let [s (assoc-in style [:style :color] (if right "#00aa00" "ff0000"))]
       [:td s
@@ -176,6 +198,7 @@
 (defn results-total
   "take results and return number of right answers and total time"
   [results]
+  ;;(print "results-total")
   (->> results
        (map
         (fn[{:keys [question user-answer time]}]
@@ -184,6 +207,7 @@
        (apply map +)))
 
 (defn results-div[]
+;;(print "results-div")
   (let [results (:results @app-state)
         results (sort
                  (fn[x y] (compare 
@@ -210,7 +234,7 @@
                 (assoc-in [:style :text-align] "center")
                 (assoc-in [:style :border-top] "black 1px solid")
                 (assoc-in [:col-span] "2"))
-            right-count " / 10"]
+            right-count " / "  quiz-length]
        [:td (-> style
                 (assoc-in [:style :text-align] "right")
                 (assoc-in [:style :border-top] "black 1px solid")
@@ -218,73 +242,81 @@
         total-time " ms"]]
       [:tr [:td {:col-span "4"} [enter-key "7vh"]]]]]))
 
-(defn progress[]
-  (let [difficulty (:difficulty @app-state)
-        question-color (fn[question]
-                         (let [d (difficulty question 1000000)]
-                           (case d
-                             1000000 "#ffffff"
-                             2000000 "#ff0000"
-                             (let [level (* 0.0512 d)]
-                               (str "rgb(" level ","
-                                    level ","
-                                    level ")")))))]
-    [:svg {:view-box "0 0 48 12"
-           :width "100%"
-           :height "120"
-           :preserve-aspect-ratio "none"
-           :xmlns "http://www.w3.org/2000/svg"}
-     (for [i (range 0 48)
-           j (range 0 12)
-           :let [op-index (quot i 12)
-                 op (["+" "*" "-" "/"] op-index)
-                 [x y] [(inc (mod i 12)) (inc j)]
-                 [x y] (case op
-                         "+" [x y]
-                         "*" [x y]
-                         "-" [(+ x y) y]
-                         "/" [(* x y) y])
-                 question {:op op :x x :y y}]]
-       ^{:key (str/join " " [i j])}
-       [:rect {:x i :y j :width "1" :height "1"
-               :fill (question-color question)}])]))
+(def progress
+  (memoize (fn[difficulty]
+             (let [question-color (fn[question]
+                                    (let [d (difficulty question 1000000)]
+                                      (case d
+                                        1000000 "#ffffff"
+                                        2000000 "#ff0000"
+                                        (let [level (* 0.0512 d)]
+                                          (str "rgb(" level ","
+                                               level ","
+                                               level ")")))))]
+               [:svg {:view-box "0 0 48 12"
+                      :width "100%"
+                      :height "120"
+                      :preserve-aspect-ratio "none"
+                      :xmlns "http://www.w3.org/2000/svg"}
+                (for [i (range 0 48)
+                      j (range 0 12)
+                      :let [op-index (quot i 12)
+                            op (["+" "*" "-" "/"] op-index)
+                            [x y] [(inc (mod i 12)) (inc j)]
+                            [x y] (case op
+                                    "+" [x y]
+                                    "*" [x y]
+                                    "-" [(+ x y) y]
+                                    "/" [(* x y) y])
+                            question {:op op :x x :y y}]]
+                  ^{:key (str/join " " [i j])}
+                  [:rect {:x i :y j :width "1" :height "1"
+                          :fill (question-color question)}])]))))
 
-(defn quiz-app []
-  (let [{:keys [user-answer quiz]} @app-state
+(defn quiz-app[]
+;;(print "quiz-app")
+  (let [{:keys [user-answer quiz difficulty]} @app-state
         question (first quiz)]
     [:div {:style {:width "100%"}}
      [:div
       (if question
         [:div {:style {:touch-action "manipulation"}}
-         [ask question user-answer]
+         [ask quiz user-answer]
          [keypad]]
         [results-div])]
-     [progress]
+     [progress difficulty]
      ]))
 
-(defn mount [el]
+(defn mount[el]
+;;(print "mount")
   (r/render-component [quiz-app] el))
 
-(defn get-app-element []
+(defn get-app-element[]
+;;(print "get-app-element")
   (.getElementById js/document "app"))
 
-(defn mount-app-element []
+(defn mount-app-element[]
+;;(print "mount-app-element")
   (mount (get-app-element)))
 
 (defn key-listener[^js/KeyboardEvent key-event]
+;;(print "key-listener")
   (let [key (.-key key-event)]
     (cond
       (#{"0" "1" "2" "3" "4" "5" "6" "7" "8" "9"} key) (type-key key)
       (= key "Backspace") (type-backspace)
       (= key "Enter") (type-enter))))
 
-(defn add-key-listener []
+(defn add-key-listener[]
+;;(print "add-key-listener")
   (.addEventListener js/document "keydown" key-listener))
 
 (defn load-listener[x]
+;;(print "load-listener")
   (mount-app-element))
 
 (defn add-load-listerner[]
+;;(print "add-load-listerner")
   (.addEventListener js/window "load" load-listener))
 
 (defonce setup-stuff
